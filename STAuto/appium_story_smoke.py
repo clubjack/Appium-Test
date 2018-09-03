58e424e7c95ec7c735c07219e9ab978ea4c986d2
clubjack <clubjack213@gmail.com>
2018-09-03 19:49:52 +0800

    增加constant

--- STAuto/appium_story_smoke.py
+++ STAuto/appium_story_smoke.py
@@ -2,24 +2,24 @@
 """
 Created on Sun Apr 15 10:58:20 2018
 
-@author: admin
+@author: chen.jiankai
 """
 #com.sixwaves.lovestory
 #com.unity3d.player.UnityPlayerActivity
 
-import unittest
 from appium import webdriver
 from time import sleep,time
 import numpy as np
 import cv2
 import os
+import constant
 
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
 #desired_caps['platformVersion'] = '7.0'
 desired_caps['deviceName'] = 'HUAWEI Mate 9'
-desired_caps['appActivity'] = 'com.unity3d.player.UnityPlayerActivity'
-desired_caps['appPackage'] = 'com.sixwaves.survivaltactics'
+desired_caps['appActivity'] = constant.appActivity
+desired_caps['appPackage'] = constant.appPackage
 #desired_caps['app'] = 'D:/Work/Stories/build/story201805211816.apk'
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
---
+++ STAuto/constant.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  3 19:45:58 2018
+
+@author: chen.jiankai
+"""
+
+appActivity = 'com.unity3d.player.UnityPlayerActivity'
+appPackage = 'com.sixwaves.survivaltactics'
