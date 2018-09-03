0deb4e15f83098fa3481641a1fe30d74048b1b1f
child1212 <li.enfeng@users.noreply.github.com>
2018-09-03 20:34:32 +0800

    fdsafsd
    fdsfsd

--- STAuto/appium_story_smoke.py
+++ STAuto/appium_story_smoke.py
@@ -23,3 +23,4 @@ desired_caps['appPackage'] = constant.appPackage
 #desired_caps['app'] = 'D:/Work/Stories/build/story201805211816.apk'
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+fdslfjsdkl
